04fbf823d981faabfd6070cc8e8cee92cf332ec9
Brandon Zeng <bzeng1234@gmail.com>
2023-04-02 13:13:23 -0400

    Initial Commit - Base NPM & Webpack Setup

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Weather_App
-The Odin Project: Weather App
+# The Odin Project: Weather App
+
+## NPM Install
+
+Run `npm install package.json` to download the required packages for development and production.
+
+## API Key
+
+You will need to create a .env file that has your API key stored to `env` for security reasons.
+.gitignore will stop the .env file from being pushed to the server.
+
+## Run Weather App (Development Mode)
+
+`npm run build-dev`
+
+## Run Weather App (Production Mode)
+
+`npm run build-prod`
---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("hello world");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDJCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlcl9hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJoZWxsbyB3b3JsZFwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
